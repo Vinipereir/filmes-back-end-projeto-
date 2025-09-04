eb26f92fca062b73f5bebe5c8f0bbc4fdbf65f11
Vinipereir <158165909+Vinipereir@users.noreply.github.com>
2025-09-04 08:55:50 -0300

    "feat:  recreation of the readme"

--- src/server.js
+++ src/server.js
@@ -17,5 +17,5 @@ app.use("/", routes);
 
 // Iniciar o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port} - Bem-vindo ao mundo dos filmes e séries!🍿`);
 });
